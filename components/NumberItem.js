2efa725ab4c7b9f608fbdb12dba32c6294f7b56e
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-06-19 21:47:41 +0700

    Add number comp

---
+++ components/NumberItem.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { colors } from '../utils/colors';
+
+const NumberItem = ({ number, pressHandler, isAnswered}) => {
+    return (
+        <TouchableOpacity
+            style={isAnswered ? [styles.roundContainer, {borderColor: colors.primary_pink}]: styles.roundContainer}
+            onPress={pressHandler}
+        >
+            <Text style={styles.text}>{number}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default NumberItem;
+
+const styles = StyleSheet.create({
+    roundContainer: {
+       
+        borderRadius: 100,
+        width: 50,
+        height: 50,
+        borderWidth: 5,
+        borderColor: 'gray',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10
+    },
+
+    text : {
+
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 14,
+        color: colors.secondary_dark_blue
+
+    }
+
+
+
+
+
+})
